62547b722a2cfc501ed79c35f6165d2497597eae
Zhang Yi <zhangyi_1357@sjtu.edu.cn>
2022-03-15 14:08:21 +0800

    Array

---
+++ Array/Array.h
@@ -0,0 +1,18 @@
+#ifndef _ARRAY_H_
+#define _ARRAY_H_
+
+template <typename T, size_t S>
+class Array {
+public:
+    constexpr int Size() const { return S; }
+
+    T& operator[](size_t index) { return m_Data[index]; }
+    const T& operator[](size_t index) const { return m_Data[index]; }
+
+    T* Data() { return m_Data; }
+    const T* Data() const { return m_Data; }
+private:
+    T m_Data[S];
+};
+
+#endif  // !_ARRAY_H_
---
+++ Array/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+
+#include "Array.h"
+
+int main() {
+    constexpr int size = 5;
+    Array<int, size> data;
+
+    static_assert(data.Size() < 10, "Size is too large");
+
+    data[0] = 2;
+    data[1] = 3;
+    data[2] = 5;
+    for (size_t i = 0; i < data.Size(); ++i)
+        std::cout << data[i] << std::endl;
+    std::cout << "-----------------" << std::endl;
+
+    memset(data.Data(), 0, data.Size() * sizeof(int));
+    for (size_t i = 0; i < data.Size(); ++i)
+        std::cout << data[i] << std::endl;
+    std::cout << "-----------------" << std::endl;
+
+    Array<std::string, size> data2;
+    data2[0] = "Cherno";
+    data2[1] = "C++";
+    for (size_t i = 0; i < data2.Size(); ++i)
+        std::cout << data2[i] << std::endl;
+
+    return 0;
+}
