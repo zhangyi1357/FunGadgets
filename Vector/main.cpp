3ea204cd3174efdd5eb00daba4cc1e7ad24202f7
Zhang Yi <zhangyi_1357@sjtu.edu.cn>
2022-03-15 18:29:45 +0800

    ~Vector API test (buggy)

--- Vector/main.cpp
+++ Vector/main.cpp
@@ -12,22 +12,29 @@ void PrintVector(const Vector<T>& vector) {
 
 class Vector3 {
 public:
-    Vector3() {}
-    Vector3(float scalar)
-        : x(scalar), y(scalar), z(scalar) {}
-    Vector3(float x, float y, float z)
-        : x(x), y(y), z(z) {}
-
-    Vector3(const Vector3& other)
-        : x(other.x), y(other.y), z(other.z) {
-        std::cout << "Copy" << std::endl;
+    Vector3() {
+        m_MemoryBlock = new int[5];
     }
-    Vector3(const Vector3&& other)
+    Vector3(float scalar)
+        : x(scalar), y(scalar), z(scalar) {
+        m_MemoryBlock = new int[5];
+    }
+    Vector3(float x, float y, float z)
+        : x(x), y(y), z(z) {
+        m_MemoryBlock = new int[5];
+    }
+
+    Vector3(const Vector3& other) = delete;
+
+    Vector3(Vector3&& other)
         : x(other.x), y(other.y), z(other.z) {
         std::cout << "Move" << std::endl;
+        m_MemoryBlock = other.m_MemoryBlock;
+        other.m_MemoryBlock = nullptr;
     }
     ~Vector3() {
         std::cout << "Destroy" << std::endl;
+        delete[] m_MemoryBlock;
     }
 
     Vector3& operator=(const Vector3& other) {
@@ -47,6 +54,8 @@ public:
     friend std::ostream& operator<<(std::ostream&, const Vector3&);
 private:
     float x = 0.0f, y = 0.0f, z = 0.0f;
+    int* m_MemoryBlock = nullptr;
+
 };
 
 std::ostream& operator<<(std::ostream& os, const Vector3& vec) {
@@ -56,14 +65,16 @@ std::ostream& operator<<(std::ostream& os, const Vector3& vec) {
 
 
 int main() {
-    Vector<Vector3> vec;
-    vec.EmplaceBack();
-    vec.EmplaceBack(1.0f);
-    vec.EmplaceBack(1.0f, 2.0f, 3.0f);
-    PrintVector(vec);
-    vec.PopBack();
-    vec.PopBack();
-    PrintVector(vec);
-
+    {
+        Vector<Vector3> vec;
+        vec.EmplaceBack();
+        vec.EmplaceBack(1.0f);
+        vec.EmplaceBack(1.0f, 2.0f, 3.0f);
+        PrintVector(vec);
+        vec.PopBack();
+        vec.PopBack();
+        PrintVector(vec);
+    }
+    std::cout << "hello" << std::endl;
     return 0;
 }
